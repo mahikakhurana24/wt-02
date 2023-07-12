734f095938eb93de93e1a1158d27527086c320fa
mahikakhurana24 <mahika.khurana.sod21@anu.edu.in>
2023-07-12 20:45:13 +0530

    updated version

---
+++ fetch.js
@@ -0,0 +1,11 @@
+
+
+let collectionsLink = document.querySelector('#Campus-link');
+let newLink = document.querySelector('#new-link');
+let trendingLink = document.querySelector('#Faculty-link');
+let popularLink = document.querySelector('#Course-link');
+
+let linkArray = [collectionsLink, newLink, trendingLink, popularLink];
+
+
+
